cb010c8b3de4e74af18c79cf572b850fee347471
ZohaibAlvi98 <zohaib.alvi90@gmail.com>
2020-07-13 19:13:12 +0500

    some steps forward

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css"
+import {BrowserRouter as Router, Route  } from "react-router-dom"
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="container">
-     hello world
-    </div>
+   <Router>
+     <Navbar />
+     <br/>
+     <Route path='/' exact component={ExercisesList} />
+     <Route path='/edit/:id' exact component={EditExercise} />
+     <Route path='/create' exact component={CreateExercise} />
+     <Route path='/user' exact component={CreateUser} />
+
+   </Router>
   );
 }
 
